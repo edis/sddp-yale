017b25615c41a2ef3a49c9d8b4e1f05e00c192e6
Edis Gonuler <edis@gonuler.com>
2022-10-05 00:25:00 -0400

    add navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,25 @@
 import Link from "next/link";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const Navbar = () => {
-    // const [isOpen, setisOpen] = useState(false);
+    const [isOpen, setisOpen] = useState(false);
+    const [isSmall, setIsSmall] = useState(false);
+
+    useEffect(() => {
+        if (isSmall) {
+            if (isOpen) {
+                const menu = document.querySelector('.menu-list-hamburger')
+                menu.style.display = "block";
+            } else {
+                const menu = document.querySelector('.menu-list-hamburger')
+                menu.style.display = "none";
+            }
+        }
+    }, [isSmall, isOpen])
 
     return (
         <nav>
+            <div className={"nav"}>
             <div>
                 <Link href={"/"}><a><img className={"logo"} alt={"SDDP Logo"} draggable={"false"} src={"/static/sddp-logo.png"} /></a></Link>
             </div>
@@ -16,6 +30,23 @@ const Navbar = () => {
                     <li className={"menu-list-item"}><Link href={"/announcements"}><a className={"link"}>Announcements</a></Link></li>
                 </ul>
             </div>
+            <div className={"hamburger"}>
+                <button onClick={() => {
+                    setIsSmall(true)
+                    setisOpen(!isOpen)
+                }}>
+                    <img src={"/static/menu.svg"} width={40} height={40} />
+                </button>
+            </div>
+            </div>
+            {/*<div>*/}
+                <ul className={"menu-list-hamburger"} style={{display: "hidden"}}>
+                    <li className={"menu-list-item"} onClick={() => setisOpen(false)}><Link href={"/"}><a className={"link"}>Home</a></Link></li>
+                    <li className={"menu-list-item"} onClick={() => setisOpen(false)}><Link href={"/team"}><a className={"link"}>Our Team</a></Link></li>
+                    <li className={"menu-list-item"} onClick={() => setisOpen(false)}><Link href={"/announcements"}><a className={"link"}>Announcements</a></Link></li>
+                </ul>
+            {/*</div>*/}
+
         </nav>
     )
 }
