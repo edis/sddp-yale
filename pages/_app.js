e702d8e6446630e1f1b5c69c9334474f78c6a2d4
Edis Gonuler <edis@gonuler.com>
2022-11-17 10:11:21 -0500

    add head to app

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,14 @@ import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
+     <>
+      <Head>
+          <meta lang={"en"} charSet="UTF-8" />
+          <meta name="viewport"
+                content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      </Head>
+
     <div>
         <Head>
             <title>SSDP Yale</title>
@@ -18,7 +26,8 @@ function MyApp({ Component, pageProps }) {
         </div>
 
     </div>
-)
+     </>
+  )
 }
 
 export default MyApp
